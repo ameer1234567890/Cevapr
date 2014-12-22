c1acc1ef4c0ce8b42febb91e6f5e1b742cebb6d8
Ameer Dawood <ameer1234567890@gmail.com>
2014-12-22 20:30:51 +0500

    v1.1

--- app/src/main/java/io/ameer/cevapr/MainActivity.java
+++ app/src/main/java/io/ameer/cevapr/MainActivity.java
@@ -1,22 +1,46 @@
 package io.ameer.cevapr;
 
 import android.app.*;
+import android.content.*;
+import android.net.*;
 import android.os.*;
 import android.view.*;
 import android.webkit.*;
-import android.widget.*;
 
 public class MainActivity extends Activity 
 {
+	WebView myWebView;
+	
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		WebView myWebView = (WebView) findViewById(R.id.webview);
+		myWebView = (WebView) findViewById(R.id.webview);
 		WebSettings webSettings = myWebView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
-		myWebView.setWebViewClient(new WebViewClient());
+		myWebView.setWebViewClient(new MyWebViewClient());
 		myWebView.loadUrl("http://ameer.io/");
     }
+	
+	@Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+	
+	private class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (Uri.parse(url).getHost().equals("ameer.io")) {
+				return false;
+			}
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+			startActivity(intent);
+			return true;
+		}
+	}
 }
+
