4408db3ec47eddd4d3e31f760706561622d74d43
Ameer Dawood <ameer1234567890@gmail.com>
2016-08-14 09:45:38 +0500

    Update deprecated API usage & remove heap size change

--- app/src/main/java/io/ameer/cevapr/BaseActivity.java
+++ app/src/main/java/io/ameer/cevapr/BaseActivity.java
@@ -3,17 +3,18 @@ package io.ameer.cevapr;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public abstract class BaseActivity extends AppCompatActivity {
     public class MyWebViewClient extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals("ameer.io")) {
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest url) {
+            if (Uri.parse(String.valueOf(url)).getHost().equals("ameer.io")) {
                 return false;
             }
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(url)));
             startActivity(intent);
             return true;
         }
